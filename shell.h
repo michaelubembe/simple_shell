41747dbb85a920af845e57c9737dd6ec6690a733
Lubembe Michael <lubemb@gmail.com>
2023-02-19 13:34:45 +0300

    added standard lib header files

--- shell.h
+++ shell.h
@@ -1,4 +1,15 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
+
 #endif
