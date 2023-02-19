550cbba09390b9d66a82e8b926013e26fd00887e
Lubembe Michael <lubemb@gmail.com>
2023-02-20 00:04:07 +0300

    added function prototypes

--- shell.h
+++ shell.h
@@ -12,13 +12,50 @@
 #include <time.h>
 #include <stdbool.h>
 
-/* prototypes */
-void prompt_user(void);
+/* environment variables */
+extern char **environ;
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
 
-/* struc functions */
+/* handle built ins */
+int checker(char **cmd, char *buf);
+void prompt_user(void);
+void handle_signal(int m);
+char **tokenizer(char *line);
+char *test_path(char **path, char *command);
+char *append_path(char *path, char *command);
+int handle_builtin(char **command, char *line);
+void exit_cmd(char **command, char *line);
+
+void print_env(void);
+
+/* string handlers */
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *s);
+char *_strchr(char *s, char c);
+
+void execution(char *cp, char **cmd);
+char *find_path(void);
+
+/* helper function for efficient free */
+void free_buffers(char **buf);
+
+struct builtin
+{
+	char *env;
+	char *exit;
+} builtin;
+
+struct info
+{
+	int final_exit;
+	int ln_count;
+} info;
+
 struct flags
 {
 	bool interactive;
 } flags;
 
-#endif
+#endif /* SHELL_H */
