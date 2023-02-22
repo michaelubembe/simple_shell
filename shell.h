49aafbddda80312757521fdfbabacaf0c84522a2
Lubembe Michael <lubemb@gmail.com>
2023-02-22 23:54:05 +0300

    fixed betty style

--- shell.h
+++ shell.h
@@ -4,64 +4,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <stddef.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <limits.h>
 #include <signal.h>
 
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
-char *_strdup(char *str);
-char *concat_all(char *name, char *sep, char *value);
+#define FALSE 0
+#define TRUE 1
+#define NEITHER 2
+#define MATCH 3
+#define PREFIX 4
+#define EXIT_SHELL 5
+#define SKIP_FORK 6
+#define DO_EXECVE 7
 
-char **splitstring(char *str, const char *delim);
-void execute(char **argv);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+/**
+ * struct Alias - singly linked list
+ * @name: name of alias
+ * @value: command that alias calls
+ * @next: points to next node
+ */
+typedef struct Alias
+{
+	char *name;
+	char *value;
+	struct Alias *next;
+} alias;
 
 extern char **environ;
 
-/**
- * struct list_path - Linked list containing PATH directories
- * @dir: directory in path
- * @p: pointer to next node
- */
-typedef struct list_path
-{
-	char *dir;
-	struct list_path *p;
-} list_path;
+extern int status;
 
+extern int line_num;
 
-char *_getenv(const char *name);
-list_path *add_node_end(list_path **head, char *str);
-list_path *linkpath(char *path);
-char *_which(char *filename, list_path *head);
+extern char *shell_name;
 
-/**
- * struct mybuild - pointer to function with corresponding buildin command
- * @name: buildin command
- * @func: execute the buildin command
- */
-typedef struct mybuild
-{
-	char *name;
-	void (*func)(char **);
-} mybuild;
+int command_manager(char **args);
 
-void(*checkbuild(char **arv))(char **arv);
+int built_ins(char **args);
+
+int and_or(char **args, char operator, int last_compare);
+
+char *check_command(char **args);
+
+int execute_command(char **args);
+
+char *input_san(char *old_buf, size_t *old_size);
+
+int input_err_check(char *ptr);
+
+void err_message(char *arg0, char *arg1);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+int _getline(char **line_ptr, size_t *n, int file);
+
+char *check_for_vars(char *arg);
+
+int _strlen(char *str);
+
+char *_strdup(char *src);
+
+char *str_concat(char *s1, char *s2);
+
+int str_compare(char *s1, char *s2, int pref_or_match);
+
+char *get_array_element(char **array, char *element_name);
+
+char **make_array(char *str, char delim, char **if_sep);
+
+int list_len(char **list, char *entry);
+
+char **array_cpy(char **old_array, int new_size);
+
+int free_array(char **args);
+
+int _setenv(const char *name, const char *value);
+
+int _unsetenv(const char *name);
+
+int change_dir(char *name);
+
+int alias_func(char **args, int free);
+
+int free_aliases(alias *alias_ptr);
+
+int check_if_alias(char **args, alias *alias_ptr);
+
+int print_aliases(alias *alias_ptr);
+
+int print_alias_value(char *arg, alias *alias_ptr);
+
+int set_alias_value(char *arg, alias *alias_ptr, char *new_value);
+
+int print_env(void);
+
+char *_itoa(int n);
+
 int _atoi(char *s);
-void exitt(char **arv);
-void env(char **arv);
-void _setenv(char **arv);
-void _unsetenv(char **arv);
-
-void freearv(char **arv);
-void free_list(list_path *head);
-
 
 #endif
